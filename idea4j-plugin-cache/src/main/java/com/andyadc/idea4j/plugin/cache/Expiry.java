4a1ce1ccbc08caec59322f74f4b854ec32977fcd
andaicheng <andaicheng@live.com>
2016-10-18 18:52:17 +0800

    optimize code

--- idea4j-plugin-cache/src/main/java/com/andyadc/idea4j/plugin/cache/Expiry.java
+++ idea4j-plugin-cache/src/main/java/com/andyadc/idea4j/plugin/cache/Expiry.java
@@ -4,17 +4,15 @@ package com.andyadc.idea4j.plugin.cache;
  * @author andaicheng
  * @version 2016/10/18
  */
-public enum Expiry {
+public class Expiry {
 
-    ETERNAL(-1L), ZERO(0), ONE_MINUTE(60 * 1000L), FIVE_MINUTES(5 * 60 * 1000L), ONE_HOUR(60 * 60 * 1000L), ONE_DAY(24 * 60 * 60 * 1000L);
+    public static final long ETERNAL = -1;
+    public static final long ZERO = 0;
+    public static final long ONE_MINUTE = 60 * 1000L;
+    public static final long FIVE_MINUTES = 5 * ONE_MINUTE;
+    public static final long TEN_MINUTES = 10 * ONE_MINUTE;
+    public static final long THIRTY_MINUTES = 30 * ONE_MINUTE;
+    public static final long ONE_HOUR = 60 * ONE_MINUTE;
+    public static final long ONE_DAY = 24 * ONE_HOUR;
 
-    private long duration;
-
-    Expiry(long duration) {
-        this.duration = duration;
-    }
-
-    public long getDuration() {
-        return duration;
-    }
 }
