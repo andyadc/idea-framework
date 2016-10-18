29e742b3e6418edb1ca749c7abb8d897064bbc20
andaicheng <andaicheng@live.com>
2016-10-18 18:45:02 +0800

    CachePlugin

---
+++ idea4j-plugin-cache/src/main/java/com/andyadc/idea4j/plugin/cache/CachePlugin.java
@@ -0,0 +1,20 @@
+package com.andyadc.idea4j.plugin.cache;
+
+import com.idea4j.framework.plugin.Plugin;
+
+/**
+ * @author andaicheng
+ * @version 2016/10/18
+ */
+public class CachePlugin implements Plugin {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
