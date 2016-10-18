9d7edce4047e0c1cc1c4b00a38889a53c4e3c2a6
andaicheng <andaicheng@live.com>
2016-10-18 10:55:57 +0800

    jdk map cache

---
+++ idea4j-plugin-cache/src/main/java/com/andyadc/idea4j/plugin/cache/impl/DefaultCache.java
@@ -0,0 +1,45 @@
+package com.andyadc.idea4j.plugin.cache.impl;
+
+import com.andyadc.idea4j.plugin.cache.Cache;
+import com.idea4j.framework.util.Assert;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author andaicheng
+ * @version 2016/10/18
+ */
+public class DefaultCache<K, V> implements Cache<K, V> {
+
+    private final Map<K, V> cashMap = new ConcurrentHashMap<>();
+
+    @Override
+    public V get(K key) {
+        Assert.notNull(key);
+        return cashMap.get(key);
+    }
+
+    @Override
+    public void put(K key, V value) {
+        Assert.notNull(key);
+        Assert.notNull(value);
+        cashMap.put(key, value);
+    }
+
+    @Override
+    public void put(K key, V value, long expiry) {
+
+    }
+
+    @Override
+    public void remove(K key) {
+        Assert.notNull(key);
+        cashMap.remove(key);
+    }
+
+    @Override
+    public void clear() {
+        cashMap.clear();
+    }
+}
