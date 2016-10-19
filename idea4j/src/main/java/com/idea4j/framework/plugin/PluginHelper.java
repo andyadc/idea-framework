ebb01c479cec0398b97045086c888b26168f7fc0
andaicheng <andaicheng@live.com>
2016-10-19 18:43:51 +0800

    Initial PluginHelper

---
+++ idea4j/src/main/java/com/idea4j/framework/plugin/PluginHelper.java
@@ -0,0 +1,18 @@
+package com.idea4j.framework.plugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 初始化插件
+ *
+ * @author andaicheng
+ * @version 2016/10/19
+ */
+public class PluginHelper {
+
+    /**
+     * 创建一个插件列表（用于存放插件实例）
+     */
+    private static final List<Plugin> PLUGIN_LIST = new ArrayList<>();
+}
