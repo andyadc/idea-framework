ad2048ff694c87cbb73e652c4b483a93791f15cf
andaicheng <andaicheng@live.com>
2016-10-19 18:41:29 +0800

    Initial DataAccessor

---
+++ idea4j/src/main/java/com/idea4j/framework/dao/DataAccessor.java
@@ -0,0 +1,10 @@
+package com.idea4j.framework.dao;
+
+/**
+ * 数据访问器
+ *
+ * @author andaicheng
+ * @version 2016/10/19
+ */
+public interface DataAccessor {
+}
---
+++ idea4j/src/main/java/com/idea4j/framework/dao/impl/DefaultDataAccessor.java
@@ -0,0 +1,14 @@
+package com.idea4j.framework.dao.impl;
+
+import com.idea4j.framework.dao.DataAccessor;
+
+/**
+ * 默认数据访问器
+ * <br/>
+ * 基于 Apache Commons DbUtils 实现
+ *
+ * @author andaicheng
+ * @version 2016/10/19
+ */
+public class DefaultDataAccessor implements DataAccessor {
+}
