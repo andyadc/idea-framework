db58c9039d5899c54ffc0f26099245c84e9ebea4
andyadc <andaicheng@gmail.com>
2016-11-05 22:14:09 +0800

    SqlHelper

--- idea4j/src/main/java/com/idea4j/framework/dao/SqlHelper.java
+++ idea4j/src/main/java/com/idea4j/framework/dao/SqlHelper.java
@@ -1,6 +1,7 @@
 package com.idea4j.framework.dao;
 
 import com.idea4j.framework.orm.EntityHelper;
+import com.idea4j.framework.util.StringUtil;
 
 /**
  * 封装 SQL 语句相关操作
@@ -10,11 +11,27 @@ import com.idea4j.framework.orm.EntityHelper;
  */
 public class SqlHelper {
 
-    private SqlHelper(){
+    private SqlHelper() {
         throw new UnsupportedOperationException();
     }
 
     private static String getTable(Class<?> entityClass) {
         return EntityHelper.getTableName(entityClass);
+    }
+
+    private static String generateWhere(String condition) {
+        String where = "";
+        if (StringUtil.isNotBlank(condition)) {
+            where += " WHERE " + condition;
+        }
+        return where;
+    }
+
+    private static String generateOrder(String sort) {
+        String order = "";
+        if (StringUtil.isNotBlank(sort)) {
+            order += " ORDER BY " + sort;
+        }
+        return order;
     }
 }
