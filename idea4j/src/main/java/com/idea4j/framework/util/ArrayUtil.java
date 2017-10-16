51b3ba7b646e410ff502c5754c101390da0d612a
andyadc <andaicheng@gmail.com>
2017-10-16 10:06:07 +0800

    optimize

--- idea4j/src/main/java/com/idea4j/framework/util/ArrayUtil.java
+++ idea4j/src/main/java/com/idea4j/framework/util/ArrayUtil.java
@@ -21,8 +21,9 @@ public final class ArrayUtil {
     public static <E> boolean contains(E[] arr, E e) {
         if (arr != null) {
             for (E e1 : arr) {
-                if (e.equals(e1))
+                if (e.equals(e1)) {
                     return true;
+                }
             }
         }
         return false;
